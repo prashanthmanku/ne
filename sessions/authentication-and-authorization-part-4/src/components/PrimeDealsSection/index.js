c3c2284772e08619e042c3a648dee8f6f7e0a376
prashanth <prasha23a@gmail.com>
2023-05-02 11:13:11 +0000

    prime,non-prime deals

--- sessions/authentication-and-authorization-part-4/src/components/PrimeDealsSection/index.js
+++ sessions/authentication-and-authorization-part-4/src/components/PrimeDealsSection/index.js
@@ -5,9 +5,17 @@ import Loader from 'react-loader-spinner'
 import ProductCard from '../ProductCard'
 import './index.css'
 
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
 class PrimeDealsSection extends Component {
   state = {
     primeDeals: [],
+    apiStatus: apiStatusConstants.initial,
   }
 
   componentDidMount() {
@@ -15,6 +23,8 @@ class PrimeDealsSection extends Component {
   }
 
   getPrimeDeals = async () => {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+
     const jwtToken = Cookies.get('jwt_token')
 
     const apiUrl = 'https://apis.ccbp.in/prime-deals'
@@ -37,7 +47,10 @@ class PrimeDealsSection extends Component {
       }))
       this.setState({
         primeDeals: updatedData,
+        apiStatus: apiStatusConstants.success,
       })
+    } else if (response.status === 401) {
+      this.setState({apiStatus: apiStatusConstants.failure})
     }
   }
 
@@ -70,7 +83,17 @@ class PrimeDealsSection extends Component {
   )
 
   render() {
-    return this.renderPrimeDealsList()
+    const {apiStatus} = this.state
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderPrimeDealsList()
+      case apiStatusConstants.failure:
+        return this.renderPrimeDealsFailureView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
   }
 }
 
